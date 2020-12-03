5915fd17ebeadb385d7f1177cb84d5e6d7987068
Noje <9hnoje@gmail.com>
2020-12-03 19:44:35 +0100

    add xcode project

---
+++ OOPsider/OOPsider/OOPsiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOPsiderApp.swift
+//  OOPsider
+//
+//  Created by noje on 03/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct OOPsiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
